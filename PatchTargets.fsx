7bff947e90050a8ee7e4474020f35554a34eabdb
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2011-10-29 00:02:29 -0700

    Refactored PatchTargets to use regular expressions instead of line-based string matching

--- PatchTargets.fsx
+++ PatchTargets.fsx
@@ -1,44 +1,37 @@
 open System.IO
+open System.Text.RegularExpressions
+
 open Microsoft.Win32
 
+let replaceRegex (r: string) (newv: string) data =
+    Regex.Replace(data, r, newv)
+
+let replaceAttributePart node attr oldv =
+    replaceRegex (sprintf "(?<=<%s[^>]*\\s%s=\"[^\"]*)%s(?=[^\"]*\")" node attr (Regex.Escape oldv))
+
+let replaceAttribute node attr oldv =
+    replaceRegex (sprintf "(?<=<%s[^>]*\\s%s=\")%s(?=\")" node attr (Regex.Escape oldv))
+
 let patchTarget source target =
-    let lines = File.ReadAllLines(source)
+    let data = File.ReadAllText(source)
 
     // Make sure file is not patched
-    if lines |> Array.exists (fun s -> s.Contains("@(ReferenceSignature)")) then
+    if data.Contains("@(ReferenceSignature)") then
         failwithf "Error patching file %s: file is already patched" source
 
-    // Insert AssemblySignature import
-    let lines = lines |> Array.map (fun s ->
-        if s = "</Project>" then "    <Import Project=\"AssemblySignature.targets\" />\r\n" + s
-        else s)
-    
-    // Make sure patching succeeded
-    if not (lines |> Array.exists (fun s -> s.Contains("AssemblySignature.targets"))) then
-        failwithf "Error patching file %s: AssemblySignature patching failed" source
-
-    // Change CoreCompile inputs to use ReferenceSignature instead of ReferencePath
-    let lines = lines |> Array.mapi (fun i s ->
-        if s.Contains("@(ReferencePath)") && lines.[i-3..i+1] |> Array.map (fun s -> s.Trim()) |> String.concat "" =
-            "@(_CoreCompileResourceInputs);$(ApplicationIcon);$(AssemblyOriginatorKeyFile);@(ReferencePath);@(CompiledLicenseFile);" then
-            s.Replace("@(ReferencePath)", "@(ReferenceSignature)")
-        else s)
-
-    // Make sure patching succeeded
-    if not (lines |> Array.exists (fun s -> s.Contains("@(ReferenceSignature);"))) then
-        failwithf "Error patching file %s: CoreCompile patching failed" source
-
-    // Patch relative imports with absolute for specific cases
-    let lines = lines |> Array.map (fun s ->
-        if s.Contains("<UsingTask") && s.Contains("AssemblyFile=\"FSharp.Build.dll\"") then
-            s.Replace("AssemblyFile=\"FSharp.Build.dll\"", "AssemblyFile=\"$(MSBuildExtensionsPath32)\\..\\Microsoft SDKs\\F#\\3.0\\Framework\\v4.0\\FSharp.Build.dll\"")
-        else if s.Contains("<Import") && s.Contains("Project=\"Microsoft.Common.targets\"") then
-            s.Replace("Project=\"Microsoft.Common.targets\"", "Project=\"$(MSBuildToolsPath)\\Microsoft.Common.targets\"")
-        else
-            s)
+    // Patch file
+    let result =
+        data
+        // Insert AssemblySignature import
+        |> replaceRegex "</Project>" "    <Import Project=\"AssemblySignature.targets\" />\r\n</Project>"
+        // Change CoreCompile inputs to use ReferenceSignature instead of ReferencePath
+        |> replaceAttributePart "Target[^>]*\\sName=\"[^\"]*Compile\"" "Inputs" "@(ReferencePath)" "@(ReferenceSignature)"
+        // Patch relative imports with absolute for specific cases
+        |> replaceAttribute "UsingTask" "AssemblyFile" "FSharp.Build.dll" @"$(MSBuildExtensionsPath32)\..\Microsoft SDKs\F#\3.0\Framework\v4.0\FSharp.Build.dll"
+        |> replaceAttribute "Import" "Project" "Microsoft.Common.targets" @"$(MSBuildToolsPath)\Microsoft.Common.targets"
 
     // Write new file
-    File.WriteAllLines(target, lines)
+    File.WriteAllText(target, result)
 
 let patchTargetLocal source =
     patchTarget source (Path.GetFileName(source))
